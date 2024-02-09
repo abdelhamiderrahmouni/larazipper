b765028911a8228a32fc0c06d31076dd4a0963b9
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2024-02-09 02:00:41 +0100

    Update zipper.sh
    made it more customizable

--- zipper.sh
+++ zipper.sh
@@ -6,16 +6,40 @@ to_snake_case() {
 }
 
 # Check if a project name was provided
-if [ "$#" -ne   1 ]; then
-    echo "Usage: $0 project_name"
+if [ "$#" -lt   1 ]; then
+    echo "Usage: $0 project_name [-x exclude_paths]"
     exit   1
 fi
 
 # Get the project name from the first argument
 project_name=$1
+shift
 
 # Convert the project name to snake case
 snake_case_name=$(to_snake_case "$project_name")
 
-# Construct the zip command using the snake case project name
-zip -r "${snake_case_name}.zip" "${project_name}/" -x "${project_name}/.git/**/*" -x "${project_name}/node_modules/**/*" -x "${project_name}/.env" -x "${project_name}/storage/**/*" -x "${project_name}/public/storage/**/*"
+# Initialize the exclude options
+exclude_options=""
+
+# Process the remaining arguments as exclude options
+while (( "$#" )); do
+    # Check if the current argument starts with '-x'
+    if [[ "$1" == "-x" ]]; then
+        # Add the exclude option to the command
+        exclude_options+=" $1"
+        shift
+        while (( "$#" )); do
+            # Add each subsequent path to the exclude options
+            exclude_options+=" ${project_name}/$1"
+            shift
+        done
+        break
+    else
+        echo "Invalid argument: $1. Expected '-x' followed by exclude paths."
+        exit   1
+    fi
+done
+
+# Construct the zip command using the snake case project name and exclude options
+zip -r "${snake_case_name}.zip" "${project_name}/"${exclude_options}
+                                              
